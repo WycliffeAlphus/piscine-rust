1e7717f92c5d6c1fa1e169e98689adeada7955c9
wyonyango <wycliffealphus7@gmail.com>
2025-04-01 15:15:11 +0300

    remove files

--- scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d
+++
@@ -1,5 +0,0 @@
-/home/wonyango/Desktop/piscine-rust/scalar/target/debug/deps/scalar-e5a77a3b9c09004d: src/lib.rs
-
-/home/wonyango/Desktop/piscine-rust/scalar/target/debug/deps/scalar-e5a77a3b9c09004d.d: src/lib.rs
-
-src/lib.rs:
