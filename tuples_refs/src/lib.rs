0c08b82551c1a4947e99c7fbd9357bd9ae251f5d
wyonyango <wycliffealphus7@gmail.com>
2025-04-02 14:21:50 +0300

    add tuples-refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,39 +1,40 @@
-pub struct Student<'a>(pub u32, pub &'a str, pub &'a str);
-
+pub struct Student<'a>(pub u32, pub &'a str, pub &'a str); 
 
 pub fn id(student: &Student) -> u32 {
     student.0
 }
 
-
 pub fn first_name(student: &Student) -> &str {
     student.1
 }
-
 
 pub fn last_name(student: &Student) -> &str {
     student.2
 }
 
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
     
-//     #[test]
-//    fn test_id(){
-//     let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-//     assert_eq!(id(&student),20 );
-//    }
+    const FIRST: &str = "Wycliffe";
+    const SECOND: &str = "Onyango";
+    
+    const STUDENT: Student = Student(20, FIRST, SECOND);
+    
+    #[test]
+    fn test_id() {
+        assert_eq!(id(&STUDENT), 20);
+    }
 
-//    #[test]
-//    fn test_first_name(){
-//     let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-//     assert_eq!(first_name(&student),"Pedro" );
-//    }
+    #[test]
+    fn test_first_name() {
+        assert_eq!(first_name(&STUDENT), "Wycliffe");
+    }
 
-//    fn test_last_name(){
-//     let student = Student(20, "Pedro".to_string(), "Domingos".to_string());
-//     assert_eq!(last_name(&student),"Domingos");
-//    }
-// }
+    #[test]
+    fn test_last_name() {
+        assert_eq!(last_name(&STUDENT), "Onyango");
+    }
+}
