abe2cc15574697b6ef4495877882cd1d9ba42587
wyonyango <wycliffealphus7@gmail.com>
2025-04-02 14:25:00 +0300

    add tuples-refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,16 +1,17 @@
-pub struct Student<'a>(pub u32, pub &'a str, pub &'a str); 
+pub struct Student<'a>(pub u32, pub &'a str, pub &'a str);
 
 pub fn id(student: &Student) -> u32 {
     student.0
 }
 
-pub fn first_name(student: &Student) -> &str {
+pub fn first_name<'a>(student: &'a Student<'a>) -> &'a str {
     student.1
 }
 
-pub fn last_name(student: &Student) -> &str {
+pub fn last_name<'a>(student: &'a Student<'a>) -> &'a str {
     student.2
 }
+
 
 
 
