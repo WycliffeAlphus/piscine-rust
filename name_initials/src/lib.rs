ceb54f72bee6ed84d90c5bc6bfe70f524e58506c
wyonyango <wycliffealphus7@gmail.com>
2025-04-03 12:54:28 +0300

    feat:update src

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,31 +1,23 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
 
-    let mut result = Vec::new();
-
-   
     for name in names {
-        
         let words: Vec<&str> = name.split_whitespace().collect();
-
         let mut initials = String::new();
 
-        
         for word in words {
             if let Some(first_char) = word.chars().next() {
-               
                 initials.push(first_char);
                 initials.push('.');
                 initials.push(' '); 
             }
         }
 
-        
         if !initials.is_empty() {
             initials.pop(); 
-     
-        result.push(initials);
+            result.push(initials);
+        }
     }
 
-    result
-}
+    result 
 }
