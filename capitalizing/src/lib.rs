b11b0f55e07e72775554ba9b914adcd793981462
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-04-05 16:20:30 +0300

    add capitalizing

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,32 @@
+pub fn capitalize_first(input: &str) -> String {
+
+    let mut chars = input.chars();
+    match chars.next(){
+        Some(first_char) => first_char.to_uppercase().collect::<String>()+chars.as_str(),
+        None => String::new(),
+    }
+}
+
+pub fn title_case(input: &str) -> String {
+
+    input
+        .split_whitespace()
+        .map(|word| capitalize_first(word))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+pub fn change_case(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| {
+
+            if c.is_lowercase(){
+                c.to_uppercase().to_string()
+            } else{
+                c.to_lowercase().to_string()
+            }
+
+        })
+        .collect::<String>()
+}
