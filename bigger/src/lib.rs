bc5983157376d68375ed9cb0afdbd2e879ba3b26
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-04-05 22:42:11 +0300

    feat:add bigger

---
+++ bigger/src/lib.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+
+    h.values()
+        .filter(|&&v| v>0)
+        .max()
+        .copied()
+        .unwrap_or(0)
+}
