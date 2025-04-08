6e4a36b6fcf854e2cdb2b38c2289ecf980f95b9f
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-04-09 00:17:03 +0300

    add profanity check

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,29 +1,30 @@
-struct Message {
+pub struct Message {
     content: String,
+    #[allow(dead_code)]
     user: String,
 }
 
 impl Message {
-    fn new(content: &str, user: &str) -> Self {
+    pub fn new(content: &str, user: &str) -> Self {
         Self {
             content: content.to_string(),
             user: user.to_string(),
         }
     }
 
-    fn send_ms(&self) -> Option<&str> {
+    pub fn send_ms(&self) -> Option<&str> {
         if self.content.is_empty() || self.content.contains("stupid") {
             None
         } else {
             Some(&self.content)
         }
     }
+}
 
-    pub fn check_ms(message: &str) -> Result<&str, &str> {
-        let msg = Messagge::new(message, "guest");
-        match msg.send_ms() {
-            Some(content) => Ok(content),
-            None => Err("ERROR:illegal"),
-        }
+pub fn check_ms<'a>(message: &'a str) -> Result<&'a str, &'static str> {
+    if message.is_empty() || message.contains("stupid") {
+        Err("ERROR: illegal")
+    } else {
+        Ok(message)
     }
 }
