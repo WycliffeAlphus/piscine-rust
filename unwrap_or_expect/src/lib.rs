8e2eb19072d34e7ed063558c27503315f18cbb7c
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-04-08 22:31:29 +0300

    feat:update fetch_data

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,39 +8,26 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match security_level {
-        Security::Unknown => {
-            match server {
-                Ok(url) => url.to_string(),
-                Err(_) => panic!(),
-            }
-        }
+        Security::Unknown => server.unwrap().to_string(),
 
-        Security::Message => {
-            match server {
-                Ok(url) => url.to_string(),
-                Err(_) => panic!("ERROR: program stops"),
-            }
-        }
+        Security::Message => match server {
+            Ok(url) => url.to_string(),
+            Err(_) => panic!("ERROR: program stops"),
+        },
 
-        Security::Warning => {
-            match server {
-                Ok(url) => url.to_string(),
-                Err(_) =>"WARNING: check the server".to_string(),
-            }
-        }
+        Security::Warning => match server {
+            Ok(url) => url.to_string(),
+            Err(_) => "WARNING: check the server".to_string(),
+        },
 
-        Security::NotFound => {
-            match server {
-                Ok(url) => url.to_string(),
-                Err(err) => format!("Not found: {}", err),
-            }
-        }
+        Security::NotFound => match server {
+            Ok(url) => url.to_string(),
+            Err(err) => format!("Not found: {}", err),
+        },
 
-        Security::UnexpectedUrl => {
-            match server {
-                Ok(url) => panic!("{}", url),
-                Err(err) => err.to_string(),
-            }
-        }
+        Security::UnexpectedUrl => match server {
+            Ok(url) => panic!("{}", url),
+            Err(err) => err.to_string(),
+        },
     }
 }
