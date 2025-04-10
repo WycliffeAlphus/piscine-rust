1d6beaedaca03d0980d364c6a6483e9638a65c13
wyonyango <wycliffealphus7@gmail.com>
2025-04-10 15:35:57 +0300

    feat:add does_it_fit

---
+++ does_it_fit/src/lib.rs
@@ -0,0 +1,41 @@
+use areas_volumes::*;
+
+pub fn area_fit(
+	x: usize,
+	y: usize,
+	objects: areas_volumes::GeometricalShapes,
+	times: usize,
+	a: usize,
+	b: usize,
+) -> bool {
+    let area = x * y;
+    let object_area: f64 = match objects {
+        areas_volumes::GeometricalShapes::Square => areas_volumes::square_area(a) as f64,
+        areas_volumes::GeometricalShapes::Circle => areas_volumes::circle_area(a),
+        areas_volumes::GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) as f64,
+        areas_volumes::GeometricalShapes::Triangle => areas_volumes::triangle_area(a, b),
+    };
+
+    (object_area * times as f64) <= area as f64
+}
+pub fn volume_fit(
+	x: usize,
+	y: usize,
+	z: usize,
+	objects: areas_volumes::GeometricalVolumes,
+	times: usize,
+	a: usize,
+	b: usize,
+	c: usize,
+) -> bool {
+    let volume = x * y * z;
+    let object_volume: f64 = match objects {
+        areas_volumes::GeometricalVolumes::Cube => areas_volumes::cube_volume(a) as f64,
+        areas_volumes::GeometricalVolumes::Sphere => areas_volumes::sphere_volume(a),
+        areas_volumes::GeometricalVolumes::Cone => areas_volumes::cone_volume(a, b),
+        areas_volumes::GeometricalVolumes::Pyramid => areas_volumes::triangular_pyramid_volume(a as f64, b),
+        areas_volumes::GeometricalVolumes::Parallelepiped => areas_volumes::parallelepiped_volume(a, b, c) as f64,
+    };
+
+    (object_volume * times as f64) <= volume as f64
+}
