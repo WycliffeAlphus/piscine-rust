f1aa445c4e326aadd23d92554e6f4b7a88b5fa4b
wyonyango <wycliffealphus7@gmail.com>
2025-04-10 16:31:53 +0300

    feat:add does-it-fit

--- does_it_fit/src/lib.rs
+++ does_it_fit/src/lib.rs
@@ -1,41 +1,84 @@
-pub mod areas_volumes;
+mod areas_volumes;
+
+pub use areas_volumes::*;
 
 pub fn area_fit(
-	x: usize,
-	y: usize,
-	objects: areas_volumes::GeometricalShapes,
-	times: usize,
-	a: usize,
-	b: usize,
+    x: usize,
+    y: usize,
+    objects: GeometricalShapes,
+    times: usize,
+    a: usize,
+    b: usize,
 ) -> bool {
-    let area = x * y;
-    let object_area: f64 = match objects {
-        areas_volumes::GeometricalShapes::Square => areas_volumes::square_area(a) as f64,
-        areas_volumes::GeometricalShapes::Circle => areas_volumes::circle_area(a),
-        areas_volumes::GeometricalShapes::Rectangle => areas_volumes::rectangle_area(a, b) as f64,
-        areas_volumes::GeometricalShapes::Triangle => areas_volumes::triangle_area(a, b),
-    };
-
-    (object_area * times as f64) <= area as f64
+    let area = rectangle_area(x, y);
+    match objects {
+        GeometricalShapes::Square => {
+            if square_area(a) * times > area {
+                return false;
+            }
+            true
+        }
+        GeometricalShapes::Circle => {
+            if circle_area(a) * times as f64 > area as f64 {
+                return false;
+            }
+            true
+        }
+        GeometricalShapes::Rectangle => {
+            if rectangle_area(a, b) * times > area {
+                return false;
+            }
+            true
+        }
+        GeometricalShapes::Triangle => {
+            if triangle_area(a, b) * times as f64 > area as f64 {
+                return false;
+            }
+            true
+        }
+    }
 }
 pub fn volume_fit(
-	x: usize,
-	y: usize,
-	z: usize,
-	objects: areas_volumes::GeometricalVolumes,
-	times: usize,
-	a: usize,
-	b: usize,
-	c: usize,
+    x: usize,
+    y: usize,
+    z: usize,
+    objects: GeometricalVolumes,
+    times: usize,
+    a: usize,
+    b: usize,
+    c: usize,
 ) -> bool {
-    let volume = x * y * z;
-    let object_volume: f64 = match objects {
-        areas_volumes::GeometricalVolumes::Cube => areas_volumes::cube_volume(a) as f64,
-        areas_volumes::GeometricalVolumes::Sphere => areas_volumes::sphere_volume(a),
-        areas_volumes::GeometricalVolumes::Cone => areas_volumes::cone_volume(a, b),
-        areas_volumes::GeometricalVolumes::Pyramid => areas_volumes::triangular_pyramid_volume(a as f64, b),
-        areas_volumes::GeometricalVolumes::Parallelepiped => areas_volumes::parallelepiped_volume(a, b, c) as f64,
-    };
-
-    (object_volume * times as f64) <= volume as f64
+    let box_v = parallelepiped_volume(x, y, z);
+    match objects {
+        GeometricalVolumes::Cube => {
+            if cube_volume(a) * times > box_v {
+                return false;
+            }
+            true
+        }
+        GeometricalVolumes::Sphere => {
+            if sphere_volume(a) * times as f64 > box_v as f64 {
+                return false;
+            }
+            true
+        }
+        GeometricalVolumes::Cone => {
+            if cone_volume(a, b) * times as f64 > box_v as f64 {
+                return false;
+            }
+            true
+        }
+        GeometricalVolumes::Pyramid => {
+            if cone_volume(a, b) * times as f64> box_v as f64 {
+                return false;
+            }
+            true
+        }
+        GeometricalVolumes::Parallelepiped => {
+            if parallelepiped_volume(a, b, c) * times > box_v {
+                return false;
+            }
+            true
+        }
+    }
 }
