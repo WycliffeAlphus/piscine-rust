abd75f8b3a3a0bbde8c2bd0534f177df1d34a70f
wyonyango <wycliffealphus7@gmail.com>
2025-04-10 14:38:27 +0300

    feat:add middle_day

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,13 @@
+use chrono::{NaiveDate, Weekday as wd}
+
+fn middle_day(year: i32)Option<wd>{
+  let is_leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+  if is_leap {
+    return None;
+  }
+  let date = NaiveDate::from_yo_opt(year, 184)?;
+  Some(date.Weekday())
+}
+
+
