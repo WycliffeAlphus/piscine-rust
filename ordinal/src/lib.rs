2c196dd154cd3f230c5267c32f35fb23ad4268d4
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-04-12 06:59:05 +0300

    feat:add ordinal

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,13 @@
+fn num_to_ordinal(n: u32) -> String {
+    let suffix = match n % 100 {
+        11 | 12 | 13 => "th",
+        _ => match n % 10 {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        },
+    };
+
+    format!("{}{}", n, suffix)
+}
