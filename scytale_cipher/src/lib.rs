61d0514f79bc757d895ce709912978f49dd66e40
wyonyango <wycliffealphus7@gmail.com>
2025-04-12 12:50:35 +0300

    feat:add scytale code

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -16,6 +16,9 @@ pub fn scytale_cipher(message: String, i: u32) -> String {
     let mut result = String::new();
     for col in 0..cols {
         for row in 0..rows {
+            if result.len() != len {
+                break;
+            }
             let ch = grid[row][col];
             result.push(ch);
         }
