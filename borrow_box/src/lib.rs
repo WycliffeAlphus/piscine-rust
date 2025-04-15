bc65e08f8282b0f66d652b34ee451e4c10bfd607
wyonyango <wycliffealphus7@gmail.com>
2025-04-15 13:49:09 +0300

    add borrow_box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,55 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u16),
+    pub p2: (String, u16),
+    pub nb_games: u16
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u16) -> Box<GameSession> {
+        
+        Box::new(GameSession{
+            id:id,
+            p1:(p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games: nb_games,
+        })
+
+    }
+    pub fn read_winner(&self) -> (String, u16) {
+
+        if self.p1.1 == self.p2.1{
+            return ("Same score! tied".to_string(), self.p1.1);
+        }
+        if self.p1.1 > self.p2.1{
+           return (self.p1.0.clone(), self.p1.1);
+        }
+        (self.p2.0.clone(), self.p2.1)
+    }
+
+
+    pub fn update_score(&mut self, user_name: String) {
+        if self.nb_games as i16 > 0 {
+            if user_name == self.p1.0{
+                self.p1.1 += 1;
+            } 
+            
+            if  user_name == self.p2.0{
+                self.p2.1 += 1;
+            }
+        }
+        if self.nb_games != 0 {
+            self.nb_games -= 1;
+        }
+        
+    }
+
+
+
+    pub fn delete(self) -> String {
+
+        format!("game deleted: id -> {}", self.id)
+
+    }
+}
