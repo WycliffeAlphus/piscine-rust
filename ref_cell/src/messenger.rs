c0364e020d3c38112f1c23c63cc5838789d3d59e
wyonyango <wycliffealphus7@gmail.com>
2025-04-16 13:38:34 +0300

    feat:add messenger.rs

--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -27,7 +27,7 @@ impl<'a> Tracker<'a> {
             self.logger.error("Error: you are over your quota!");
         } else if percent >= 70.0 {
             self.logger.warning(&format!(
-                "Warning: you have used up over {:.0}% of your quota! Proceeds with precaution",
+                "you have used up over {:.0}% of your quota! Proceeds with precaution",
                 percent
             ));
         }
@@ -37,7 +37,7 @@ impl<'a> Tracker<'a> {
         let count = std::rc::Rc::strong_count(tracked);
         let percent = (count as f64 / self.max as f64) * 100.0;
         self.logger.info(&format!(
-            "Info: you are using up to {:.0}% of your quota",
+            "you are using up to {:.0}% of your quota",
             percent
         ));
     }
