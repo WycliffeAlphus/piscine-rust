43db0ecd3a59afa89249aa9bb7fd9dfc59d7b223
wyonyango <wycliffealphus7@gmail.com>
2025-04-16 15:08:43 +0300

    feat:update refcell

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -1,49 +1,43 @@
-
-use std::collections::HashMap;
+pub use std::collections::HashMap;
 pub use std::cell::RefCell;
 pub use std::rc::Rc;
 
-mod messenger;
-pub use messenger::{Logger, Tracker};
+pub use messenger::*;
 
+pub mod messenger;
+
+#[derive(Debug)]
 pub struct Worker {
-    pub track_value: Rc<u64>,
+    pub track_value: Rc<usize>,
     pub mapped_messages: RefCell<HashMap<String, String>>,
     pub all_messages: RefCell<Vec<String>>,
 }
 
 impl Worker {
-    pub fn new(value: u64) -> Self {
-        Self {
-            track_value: Rc::new(value),
+pub    fn new(num:usize) -> Worker {
+        Worker {
+            track_value: Rc::new(num),
             mapped_messages: RefCell::new(HashMap::new()),
             all_messages: RefCell::new(Vec::new()),
         }
     }
+  
 }
 
 impl Logger for Worker {
-    fn warning(&self, msg: &str) {
-        let formatted = format!("Warning: {}", msg);
-        self.mapped_messages
-            .borrow_mut()
-            .insert("Warning".to_string(), formatted.clone());
-        self.all_messages.borrow_mut().push(formatted);
+   fn info(&self, msg: &str) {
+        self.mapped_messages.borrow_mut().insert("Info".to_owned(),msg.to_owned().drain(6..msg.len()).collect());
+        self.all_messages.borrow_mut().push(format!("{}",msg.to_owned()));
     }
-
-    fn info(&self, msg: &str) {
-        let formatted = format!("Info: {}", msg);
-        self.mapped_messages
-            .borrow_mut()
-            .insert("Info".to_string(), formatted.clone());
-        self.all_messages.borrow_mut().push(formatted);
-    }
-
     fn error(&self, msg: &str) {
-        let formatted = format!("Error: {}", msg);
-        self.mapped_messages
-            .borrow_mut()
-            .insert("Error".to_string(), formatted.clone());
-        self.all_messages.borrow_mut().push(formatted);
+        self.mapped_messages.borrow_mut().insert("Error".to_owned(),msg.to_owned().drain(7..msg.len()).collect());
+        self.all_messages.borrow_mut().push(format!("{}",msg.to_owned()));
+
+    }
+    fn warning(&self, msg: &str) {
+        self.mapped_messages.borrow_mut().insert("Warning".to_owned(),msg.to_owned().drain(9..msg.len()).collect());
+        self.all_messages.borrow_mut().push(format!("{}",msg.to_owned()));
+
     }
 }
+
--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -1,44 +1,43 @@
-pub trait Logger {
-    fn warning(&self, msg: &str);
+pub use std:: rc::Rc;
+
+
+pub struct Tracker<'a> {
+    pub logger: &'a dyn Logger,
+    pub value: Rc<i32>,
+    pub max: i32,
+}
+
+pub trait Logger  {
     fn info(&self, msg: &str);
     fn error(&self, msg: &str);
+    fn warning(&self, msg: &str);
 }
-#[allow(dead_code)]
-pub struct Tracker<'a> {
-    logger: &'a dyn Logger,
-    value: usize,
-    max: usize,
-}
-#[allow(dead_code)]
-impl<'a> Tracker<'a> {
-    pub fn new(logger: &'a dyn Logger, max: usize) -> Self {
-        Tracker {
+
+impl <'a>Tracker<'a> {
+
+  pub  fn new(logger: &'a dyn Logger ,num:i32) -> Tracker<'a> {
+
+       Tracker{
             logger,
-            value: 0,
-            max,
+            value: Rc::new(num),
+            max:num,
         }
     }
+   pub  fn set_value(&self,num: &Rc<usize>) {
     
-    pub fn set_value<T>(&self, tracked: &std::rc::Rc<T>) {
-        let count = std::rc::Rc::strong_count(tracked);
-        let percent = ((count as f64 / self.max as f64) * 100.0).floor();
-    
-        if percent >= 100.0 {
-            self.logger.error("you are over your quota!");
-        } else if percent >= 70.0 {
-            let msg = format!(
-                "you have used up over {:.0}% of your quota! Proceeds with precaution",
-                percent
-            );
-            self.logger.warning(&msg);
+        let count = Rc::strong_count(num);
+        let percent = (count * 100) / self.max as usize;
+        if percent >= 70 &&percent<100{
+            self.logger.warning( format!("Warning: you have used up over {}% of your quota! Proceeds with precaution", percent).as_str());
+        }
+        if percent >= 100 {
+            self.logger.error( "Error: you are over your quota!");
         }
     }
-    
-    pub fn peek<T>(&self, tracked: &std::rc::Rc<T>) {
-        let count = std::rc::Rc::strong_count(tracked);
-        let percent = ((count as f64 / self.max as f64) * 100.0).floor();
-        let msg = format!("Info: you are using up to {:.0}% of your quota", percent);
-        self.logger.info(&msg);
+    pub fn peek(&self,num: &Rc<usize>) {
+
+        let count = Rc::strong_count(&num);
+        let percent = (count * 100) / self.max as usize;
+        self.logger.info(format!("Info: you are using up to {}% of your quota", percent).as_str());
     }
-    
 }
