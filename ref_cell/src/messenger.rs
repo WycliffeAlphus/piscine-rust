decc7986feb31419f6b079c964e85feb4c877b16
wyonyango <wycliffealphus7@gmail.com>
2025-04-16 14:54:26 +0300

    add lib.rs messenger

--- ref_cell/src/messenger.rs
+++ ref_cell/src/messenger.rs
@@ -22,18 +22,23 @@ impl<'a> Tracker<'a> {
     pub fn set_value<T>(&self, tracked: &std::rc::Rc<T>) {
         let count = std::rc::Rc::strong_count(tracked);
         let percent = ((count as f64 / self.max as f64) * 100.0).floor();
+    
         if percent >= 100.0 {
             self.logger.error("you are over your quota!");
         } else if percent >= 70.0 {
-            
-            self.logger.warning("you have used up over {}% of your quota! Proceeds with precaution",percent);
+            let msg = format!(
+                "you have used up over {:.0}% of your quota! Proceeds with precaution",
+                percent
+            );
+            self.logger.warning(&msg);
         }
     }
     
     pub fn peek<T>(&self, tracked: &std::rc::Rc<T>) {
         let count = std::rc::Rc::strong_count(tracked);
         let percent = ((count as f64 / self.max as f64) * 100.0).floor();
-        
-        self.logger.info("you are using up to {}% of your quota",percent);
+        let msg = format!("Info: you are using up to {:.0}% of your quota", percent);
+        self.logger.info(&msg);
     }
+    
 }
