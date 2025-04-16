94082912cf57378085d105ad3852f5db2ccdd804
wyonyango <wycliffealphus7@gmail.com>
2025-04-16 13:24:01 +0300

    feat:add ref_cell

---
+++ ref_cell/src/lib.rs
@@ -0,0 +1,46 @@
+
+use std::collections::HashMap;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+mod messenger;
+pub use messenger::{Logger, Tracker};
+
+pub struct Worker {
+    pub track_value: Rc<u8>,
+    pub mapped_messages: RefCell<HashMap<String, String>>,
+    pub all_messages: RefCell<Vec<String>>,
+}
+
+impl Worker {
+    pub fn new(value: u8) -> Self {
+        Self {
+            track_value: Rc::new(value),
+            mapped_messages: RefCell::new(HashMap::new()),
+            all_messages: RefCell::new(Vec::new()),
+        }
+    }
+}
+
+impl Logger for Worker {
+    fn warning(&self, msg: &str) {
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Warning".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(format!("Warning: {}", msg));
+    }
+
+    fn info(&self, msg: &str) {
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Info".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(format!("Info: {}", msg));
+    }
+
+    fn error(&self, msg: &str) {
+        self.mapped_messages
+            .borrow_mut()
+            .insert("Error".to_string(), msg.to_string());
+        self.all_messages.borrow_mut().push(format!("Error: {}", msg));
+    }
+}
