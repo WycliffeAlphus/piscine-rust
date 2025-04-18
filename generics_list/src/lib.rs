44f6753efccdc250bcfad0c35b314b263476ce50
wyonyango <wycliffealphus7@gmail.com>
2025-04-18 14:48:14 +0300

    feat:update lib.rs

--- generics_list/src/lib.rs
+++ generics_list/src/lib.rs
@@ -1,40 +1,42 @@
 #[derive(Clone, Debug)]
 pub struct List<T> {
-    pub head: Option<Box<Node<T>>>,  
+    pub head: Option<Node<T>>,
 }
 
 #[derive(Clone, Debug)]
 pub struct Node<T> {
     pub value: T,
-    pub next: Option<Box<Node<T>>>,  
+    pub next: Option<Box<Node<T>>>,
 }
 
-impl<T: Clone> List<T> {
+impl<T> List<T> {
     pub fn new() -> List<T> {
         List { head: None }
     }
 
     pub fn push(&mut self, value: T) {
-        let new = Node {
+        let new_node = Node {
             value,
-            next: self.head.take(), 
+            next: self.head.take().map(Box::new),
         };
-        self.head = Some(Box::new(new));
+        self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) {
         if let Some(node) = self.head.take() {
-            self.head = node.next;
+            self.head = node.next.map(|boxed| *boxed);
         }
     }
 
     pub fn len(&self) -> usize {
         let mut count = 0;
-        let mut current = self.head.as_ref();
+        let mut current = &self.head;
+
         while let Some(node) = current {
             count += 1;
-            current = node.next.as_ref();
+            current = &node.next.as_deref();
         }
+
         count
     }
 }
