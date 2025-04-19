a3a2667b6d7b716081c0c4609703b960787cb894
wyonyango <wycliffealphus7@gmail.com>
2025-04-19 13:49:08 +0300

    update lalgebra

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,78 +1,46 @@
-use std::ops::{Add, Mul};
-use std::iter::Sum;
-use std::clone::Clone;
-use std::cmp::PartialEq;
 use std::fmt::Debug;
+use std::ops::{Add, Mul};
 
+// Trait bounds: T needs to support basic operations
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Vector<T: Scalar>(pub Vec<T>);
 
-pub trait Scalar: 
-    Add<Output = Self> + 
-    Mul<Output = Self> + 
-    Sum + 
-    Clone + 
-    Debug + 
-    PartialEq + 
-    Eq 
-{}
-
-impl<T> Scalar for T where
-    T: Add<Output = T> + 
-       Mul<Output = T> + 
-       Sum + 
-       Clone + 
-       Debug + 
-       PartialEq + 
-       Eq 
-{}
-
-impl<T: Scalar> Add for Vector<T> {
-    type Output = Self;
-
-    fn add(self, other: Self) -> Self::Output {
-        if self.0.len() != other.0.len() {
-            panic!("Vectors must be of the same length for addition.");
-        }
-
-        let data = self.0.into_iter()
-                         .zip(other.0.into_iter())
-                         .map(|(a, b)| a + b)
-                         .collect();
-        Vector(data)
-    }
-}
+// Trait alias for all the capabilities T must have
+pub trait Scalar: Copy + Add<Output = Self> + Mul<Output = Self> + Debug + PartialEq + Eq {}
+impl<T> Scalar for T where T: Copy + Add<Output = T> + Mul<Output = T> + Debug + PartialEq + Eq {}
 
 impl<T: Scalar> Vector<T> {
     pub fn new() -> Self {
         Vector(Vec::new())
     }
 
- 
     pub fn dot(&self, other: &Self) -> Option<T> {
         if self.0.len() != other.0.len() {
             return None;
         }
 
-        Some(
-            self.0.iter()
-                 .cloned()
-                 .zip(other.0.iter().cloned())
-                 .map(|(a, b)| a * b)
-                 .sum()
-        )
+        let mut result = self.0[0] * other.0[0];
+        for i in 1..self.0.len() {
+            result = result + self.0[i] * other.0[i];
+        }
+        Some(result)
     }
+}
 
-    pub fn try_add(&self, other: &Self) -> Option<Self> {
+// Adding two vectors
+impl<T: Scalar> Add for Vector<T> {
+    type Output = Option<Vector<T>>;
+
+    fn add(self, other: Self) -> Option<Vector<T>> {
         if self.0.len() != other.0.len() {
             return None;
         }
 
-        let data = self.0.iter()
-                         .cloned()
-                         .zip(other.0.iter().cloned())
-                         .map(|(a, b)| a + b)
-                         .collect();
-        Some(Vector(data))
+        let result: Vec<T> = self.0.iter()
+            .zip(other.0.iter())
+            .map(|(a, b)| *a + *b)
+            .collect();
+
+        Some(Vector(result))
     }
 }
