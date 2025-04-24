1bce98a05c38373cda90141fa03a532a21617fc7
wyonyango <wycliffealphus7@gmail.com>
2025-04-24 09:56:45 +0300

    feat:add cargo toml file

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
+}
+
+#[allow(elided_named_lifetimes)]
+impl Person<'_> {
+	pub fn new<'a>(name: &'a str) -> Person{
+        Person {
+            name: name,
+            age: 0,
+        }
+	}
+}
