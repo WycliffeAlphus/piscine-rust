f985c07e1d1013986ce5e7493ced1b6026b5b5a5
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-04-26 22:54:49 +0300

    fiz:import std::hash::Hash

--- slices_to_map/src/lib.rs
+++ slices_to_map/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use std::hash::Hash;
 pub fn slices_to_map<'a, T: Hash + Eq, U>(keys: &'a [T], values: &'a [U]) -> HashMap<&'a T, &'a U> {
     let mut map = HashMap::new();
     for (k, v) in keys.iter().zip(values.iter()) {
